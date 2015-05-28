3a5733e950ea3c1a1793827d9034f09f054a984e
Nikolay Ignatyev <noilgt@gmail.com>
2015-05-28 15:17:48 +0300

    Added sqlite db
    config.py - db folder, migration file path
    db_create.py - main db init
    app.db - DataBase

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#folder with db file
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#folder with migration 
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'VeryH4rdT00Gue55'
